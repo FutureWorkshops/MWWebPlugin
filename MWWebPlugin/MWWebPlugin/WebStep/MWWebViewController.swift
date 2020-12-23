aef8d33c4f203772863084dcd5656ab4e0cc698b
Xavi Moll <xmollv@gmail.com>
2020-12-23 12:27:06 +0100

    Added the base for the UIViewController

--- MWWebPlugin/MWWebPlugin/WebStep/MWWebViewController.swift
+++ MWWebPlugin/MWWebPlugin/WebStep/MWWebViewController.swift
@@ -6,8 +6,30 @@
 //
 
 import Foundation
+import SafariServices
 import MobileWorkflowCore
 
-public class MWWebViewController: UIViewController {
+public class MWWebViewController: ORKStepViewController {
+    
+    private var webStep: MWWebStep {
+        guard let webStep = self.step as? MWWebStep else {
+            preconditionFailure("Unexpected step type. Expecting \(String(describing: MWWebStep.self)), got \(String(describing: type(of: self.step)))")
+        }
+        return webStep
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupWebView()
+        self.loadWebView()
+    }
+    
+    private func setupWebView() {
+        
+    }
+    
+    private func loadWebView() {
+        
+    }
     
 }
