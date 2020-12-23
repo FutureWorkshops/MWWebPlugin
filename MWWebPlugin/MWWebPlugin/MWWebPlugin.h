de3e9b5957b92161b9e1c553a1b86d024a6ba037
Xavi Moll <xmollv@gmail.com>
2020-12-23 12:10:47 +0100

    Added the plugin project

---
+++ MWWebPlugin/MWWebPlugin/MWWebPlugin.h
@@ -0,0 +1,18 @@
+//
+//  MWWebPlugin.h
+//  MWWebPlugin
+//
+//  Created by Xavi Moll on 23/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MWWebPlugin.
+FOUNDATION_EXPORT double MWWebPluginVersionNumber;
+
+//! Project version string for MWWebPlugin.
+FOUNDATION_EXPORT const unsigned char MWWebPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWWebPlugin/PublicHeader.h>
+
+
