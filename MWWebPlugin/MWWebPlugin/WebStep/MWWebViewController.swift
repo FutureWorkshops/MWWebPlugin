bc20edbcae05573d65054ff1337266f0f752d47c
Xavi Moll <xmollv@gmail.com>
2020-12-23 15:26:08 +0100

    Removed SFSafariViewController and replaced it for a WKWebView implementation

--- MWWebPlugin/MWWebPlugin/WebStep/MWWebViewController.swift
+++ MWWebPlugin/MWWebPlugin/WebStep/MWWebViewController.swift
@@ -5,12 +5,13 @@
 //  Created by Xavi Moll on 23/12/20.
 //
 
+import WebKit
 import Foundation
-import SafariServices
 import MobileWorkflowCore
 
 public class MWWebViewController: ORKStepViewController {
     
+    private let webView = WKWebView()
     private var webStep: MWWebStep {
         guard let webStep = self.step as? MWWebStep else {
             preconditionFailure("Unexpected step type. Expecting \(String(describing: MWWebStep.self)), got \(String(describing: type(of: self.step)))")
@@ -20,8 +21,65 @@ public class MWWebViewController: ORKStepViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        let sfs = SFSafariViewController(url: self.webStep.url)
-        self.addCovering(childViewController: sfs)
+        self.setupWebView()
+        self.load(url: self.webStep.url)
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setToolbarHidden(false, animated: animated)
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setToolbarHidden(true, animated: animated)
+    }
+    
+    //MARK: Private methods
+    private func setupWebView() {
+        self.webView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.view.addSubview(self.webView)
+        self.setToolbarItems([
+            UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(self.navigateBack(_:))),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "chevron.forward"), style: .plain, target: self, action: #selector(self.navigateForward(_:))),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain, target: self, action: #selector(self.reloadCurrentPageOrOriginal(_:))),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Continue", style: .done, target: self, action: #selector(self.continueToNextStep(_:)))
+        ], animated: false)
+    }
+    
+    private func load(url: URL) {
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 60)
+        self.webView.load(request)
+    }
+    
+    //MARK: IBActions
+    @IBAction private func navigateBack(_ sender: UIBarButtonItem) {
+        if self.webView.canGoBack {
+            self.webView.goBack()
+        }
+    }
+    
+    @IBAction private func navigateForward(_ sender: UIBarButtonItem) {
+        if self.webView.canGoForward {
+            self.webView.goForward()
+        }
+    }
+    
+    @IBAction private func reloadCurrentPageOrOriginal(_ sender: UIBarButtonItem) {
+        let urlToReload: URL
+        if let currentUrl = self.webView.url {
+            urlToReload = currentUrl
+        } else {
+            urlToReload = self.webStep.url
+        }
+        self.load(url: urlToReload)
+    }
+    
+    @IBAction private func continueToNextStep(_ sender: UIBarButtonItem) {
+        self.goForward()
     }
     
 }
