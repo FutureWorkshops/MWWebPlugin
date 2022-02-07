997eb1f927e5b91b4685412ffb5d2a1dbe618c43
Igor Ferreira <me@igorcferreira.dev>
2022-02-07 15:27:39 -0300

    Bump version to 0.1.0

---
+++ scripts/align_plugin_files.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+
+#Source reference: https://stackoverflow.com/a/53208376
+#Reference author: Shuangquan Wei
+#Reference author profile: https://stackoverflow.com/users/5445143/shuangquan-wei
+
+require 'xcodeproj'
+
+def is_resource(file)
+    extname= file[/\.[^\.]+$/]
+    if extname == '.bundle' || extname == '.xcassets' then
+        return true
+    else
+        return false
+    end
+end
+
+def align_files(project, target, group)
+
+    if File.exist?(group.real_path) 
+
+        Dir.foreach(group.real_path) do |entry|
+            filePath = File.join(group.real_path, entry)
+            if filePath.to_s.end_with?(".DS_Store", ".xcconfig") then
+                puts "Ignoring configuration file " + filePath
+            elsif filePath.to_s.end_with?(".lproj") then
+                if @variant_group.nil?
+                    @variant_group = group.new_variant_group("Localizable.strings");
+                end
+                string_file = File.join(filePath, "Localizable.strings")
+                puts "Adding strings file " + string_file
+                fileReference = @variant_group.new_reference(string_file)
+                target.add_resources([fileReference])
+            elsif is_resource(entry) then
+                puts "Adding resource " + filePath
+                fileReference = group.new_reference(filePath)
+                target.add_resources([fileReference])
+            elsif !File.directory?(filePath) then
+                fileReference = group.new_reference(filePath)
+                if filePath.to_s.end_with?(".m", ".mm", ".cpp", ".swift") then
+                    puts "Adding source " + filePath
+                    target.add_file_references([fileReference])
+
+                elsif filePath.to_s.end_with?(".pch") then
+
+                elsif filePath.to_s.end_with?("Info.plist") && entry == "Info.plist" then
+
+                elsif filePath.to_s.end_with?(".h") then
+                    # target.headers_build_phase.add_file_reference(fileReference)
+                elsif filePath.to_s.end_with?(".framework") || filePath.to_s.end_with?(".a") then
+                    puts "Adding framework " + filePath
+                    target.frameworks_build_phases.add_file_reference(fileReference)
+                elsif
+                    puts "Adding resource " + filePath
+                    target.add_resources([fileReference])
+                end
+            
+            elsif File.directory?(filePath) && entry != '.' && entry != '..' then
+                puts "Creating subgroup " + entry 
+                subGroup = group.find_subpath(entry, true)
+                subGroup.set_source_tree(group.source_tree)
+                subGroup.set_path(File.join(group.real_path, entry))
+                align_files(project, target, subGroup)
+
+            end
+        end
+    end
+end
+
+target_name = ARGV[0].to_s
+project_path = './' + target_name + '/' + target_name + '.xcodeproj'
+project = Xcodeproj::Project.open(project_path)
+group = project[target_name]
+
+project.targets.each do |target|
+	if target.name == target_name
+        puts 'Removing resources'
+		target.source_build_phase.files.to_a.map do |file|
+			file.remove_from_project
+		end
+        puts 'Removing sources'
+		target.resources_build_phase.files.to_a.map do |file|
+			file.remove_from_project
+		end
+        puts 'Cleaning group'
+		group.clear
+        puts 'Aligning files'
+		align_files(project, target, group)
+	end
+end
+
+project.save
---
+++ scripts/build_framework.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+BUILD_DIR="$(pwd)/tmp"
+DESTINATION_DIR="$(pwd)/Build"
+CONFIGURATION="Release"
+WORKSPACE=""
+TARGET=""
+IS_CI="false"
+
+# This function does the common code
+# $1 is the scheme to build:
+# Example: build MobileWorkflowCore Release
+function archive() {
+    SCHEME="$1"
+    BUILD_CONFIGURATION="$2"
+
+    xcodebuild -workspace "${WORKSPACE}" \
+               -scheme "${SCHEME}" \
+               -configuration ${BUILD_CONFIGURATION} \
+               -sdk iphoneos \
+               -archivePath "${BUILD_DIR}/device/${SCHEME}" \
+               archive \
+               SKIP_INSTALL=NO \
+               BUILD_LIBRARY_FOR_DISTRIBUTION=YES && \
+    xcodebuild -workspace "${WORKSPACE}" \
+               -scheme "${SCHEME}" \
+               -sdk iphonesimulator \
+               -configuration ${BUILD_CONFIGURATION} \
+               -archivePath "${BUILD_DIR}/simulator/${SCHEME}" \
+               archive \
+               SKIP_INSTALL=NO \
+               BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+}
+
+function compose() {
+  ARCHIVE_NAME="$1"
+  FRAMEWORK_NAME="$2"
+  FRAMEWORK_PATH="$DESTINATION_DIR/$FRAMEWORK_NAME.xcframework"
+  DEVICE_FRAMEWORK="$BUILD_DIR/device/$ARCHIVE_NAME.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework"
+  SIMULATOR_FRAMEWORK="$BUILD_DIR/simulator/$ARCHIVE_NAME.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework"
+
+  xcodebuild -create-xcframework \
+  -framework $DEVICE_FRAMEWORK \
+  -framework $SIMULATOR_FRAMEWORK \
+  -output $FRAMEWORK_PATH
+}
+
+while [ -n "$1" ]; do
+    case "$1" in
+        --ci) IS_CI="true";;
+        --workspace) WORKSPACE="$2" && shift;;
+        --target) TARGET="$2" && shift;;
+    esac
+    shift
+done
+
+rm -rf $DESTINATION_DIR
+mkdir $DESTINATION_DIR
+
+rm -rf "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+
+archive $TARGET $CONFIGURATION
+
+compose $TARGET $TARGET
+
+rm -rf $BUILD_DIR
+
+if [[ "${IS_CI}" = "false" ]]; then
+  open $DESTINATION_DIR
+fi
